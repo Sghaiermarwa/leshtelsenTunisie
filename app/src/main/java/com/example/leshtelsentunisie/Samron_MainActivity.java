2d07256902e1442494fef26cd52693a87e7b3b73
Sghaiermarwa <sghaierrouroumarwa@gmail.com>
2019-12-02 16:15:20 +0100

    j'ai ajouté la configuration de google maps API key et j'ai realisé la 4eme interface (carte graphique pour le premier hotel)

--- app/src/main/java/com/example/leshtelsentunisie/Samron_MainActivity.java
+++ app/src/main/java/com/example/leshtelsentunisie/Samron_MainActivity.java
@@ -2,7 +2,10 @@ package com.example.leshtelsentunisie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Samron_MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Samron_MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_samron__main);
+        ImageView img = (ImageView) findViewById(R.id.loc);
+        img.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               // le click sur l'icone de lacalisation entraine l'ouverture d'une autre activity google maps
+                Intent newActivity1 = new Intent(Samron_MainActivity.this, MapsActivitySamron.class);
+                startActivity(newActivity1);
+            }
+        });
+
     }
 }
